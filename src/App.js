8cf4d06146149d0260b45e3674c809e6fbcdc52e
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2024-06-06 17:14:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {Route,Routes} from 'react-router-dom';
 import './App.css';
+import About from './Components/Pages/About';
+import Services from './Components/Pages/Services';
+import Dashboard from './Components/Pages/Dashboard';
+import Contact from './Components/Pages/Contact';
+import Navbar from './Components/Navbar/Navbar';
+import { Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar/>
+    <Routes>
+        <Route path='*' element={<Navigate to="/" />} />
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Services" element={<Services/>} />
+        <Route path="/Contact" element={<Contact/>} />
+    </Routes>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+// import logo from '../../../public/Icons/logo.png';
+import classes from './Navbar.module.css';
+
+const pages = ['About', 'Services', 'Contact'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const navigate=useNavigate();
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const navigation = (path) => {
+    console.log("path---------->",path)
+  if(path){
+    navigate('/'+path);
+  }else{
+    navigate('/');
+  }
+  }
+
+  return (
+   <div className={classes.navbar}>
+      <div className={classes.logo_container}>
+        <img className={classes.logo} src='Icons/logo.png' alt="logo" onClick={()=>navigation('')}/>
+      </div>
+      <div className={classes.navlink_container}>
+        <ul>
+          <li><button class={classes.button2} onClick={()=>navigation('About')}>About</button></li>
+          <li><button class={classes.button2} onClick={()=>navigation('Services')}>Services</button></li>
+          <li><button class={classes.button2} onClick={()=>navigation('Contact')}>Contact</button></li>
+        </ul>
+      </div>
+   </div>
+  );
+}
+export default Navbar;
