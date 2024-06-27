0c357b46b46e00b739b6b9fb2bf0b87f1f5325dd
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2024-06-27 15:55:05 +0530

    updated code

--- src/Components/Pages/Enroll.jsx
+++ src/Components/Pages/Enroll.jsx
@@ -10,41 +10,15 @@ const Enroll = () => {
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: .3, ease: "easeInOut" }}
     >
-        <div className={classes.enroll}>
-    <h2>CHOOSE YOUR FITNESS </h2>
-    <h2>MEMBERSHIP PLAN</h2>
-        <div className={classes.card_container}>
-        <Card plan="BASIC"  price='2000' month="1 Month" description="Unlimited access to all area." />
-        <Card plan="SILVER" price='1500' month="3 Month" description="Unlimited access to all area." />
-        <Card plan="GOLD" price='1200' month="6 Month" description="Unlimited access to all area." />
-        <Card plan="PLATINUM" price='1000' month="12 Month" description="Unlimited access to all area." />
-
-
-            {/* <div className={classes.card}>
-                BASIC 
-                <p>2000<span className={classes.month}>&#8377;/month</span></p>
-                <p>Duration - 1 Month</p>
+    <div className={classes.enroll}>
+        <h2>CHOOSE YOUR FITNESS </h2>
+        <h2>MEMBERSHIP PLAN</h2>
+            <div className={classes.card_container}>
+                <Card plan="BASIC"  price='2000' month="1 Month" description="Unlimited access to all area." />
+                <Card plan="SILVER" price='1500' month="3 Month" description="Unlimited access to all area." />
+                <Card plan="GOLD" price='1200' month="6 Month" description="Unlimited access to all area." />
+                <Card plan="PLATINUM" price='1000' month="12 Month" description="Unlimited access to all area." />
             </div>
-
-            <div className={classes.card}>
-                SILVER
-                <p>1500<span className={classes.month}>&#8377;/month</span></p>
-                <p>Duration - 3 Months</p>
-            </div>
-
-            <div className={classes.card}>
-                GOLD
-                <p>1200<span className={classes.month}>&#8377;/month</span></p>
-                <p>Duration - 6 Months</p>
-            </div>
-
-            <div className={classes.card}>
-                PLATINUM
-                <p>1000<span className={classes.month}>&#8377;/month</span></p>
-                <p>Duration - 12 Months</p>
-
-            </div> */}
-        </div>
     </div>
      </motion.div>
   )
