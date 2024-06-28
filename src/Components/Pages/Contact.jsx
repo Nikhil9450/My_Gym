fa6f5a98512a528d9e11e791a2e2bb6f73fc9990
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2024-06-28 17:08:25 +0530

    updated code

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
-
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import classes from './Contact.module.css'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={classes.container}>
+    <div className={classes.leftdiv}>
+
+    </div>
+    <div className={classes.rightdiv}>
+
+    <div className={classes['input-group']}>
+          <input
+            required
+            type="text"
+            name="text"
+            autoComplete="off"
+            className={classes.input}
+          />
+          <label className={classes['user-label']}>Your Name</label>
+      </div>
+      <div className={classes['input-group']}>
+          <input
+            required
+            type="text"
+            name="text"
+            autoComplete="off"
+            className={classes.input}
+          />
+          <label className={classes['user-label']}>Your Email</label>
+      </div>
+      <div className={classes['input-group']}>
+          <input
+            required
+            type="text"
+            name="text"
+            autoComplete="off"
+            className={classes.input}
+          />
+          <label className={classes['user-label']}>Subject</label>
+      </div>
+      <div className={classes['input-group']}>
+        <textarea
+          required
+          name="text"
+          autoComplete="off"
+          className={classes.textarea}
+        />
+        <label className={classes['user-label']}>Message</label>
+      </div>
+        <button className={classes.btn}>Send</button>
+    </div>
+    </div>
   )
 }
 
